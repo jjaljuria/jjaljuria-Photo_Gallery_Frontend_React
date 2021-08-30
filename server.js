eebcf20041cfb086f8563d711199eaa20a32d073
José Aljuria <josejavieral13@gmail.com>
2021-08-30 09:20:50 -0400

    created server.js static server

---
+++ server.js
@@ -0,0 +1,15 @@
+const handler = require('serve-handler');
+const http = require('http');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+	return handler(req, res, {
+		public: path.join(__dirname, 'build')
+	});
+})
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+	console.log('Server running on port: ' + port);
+});
