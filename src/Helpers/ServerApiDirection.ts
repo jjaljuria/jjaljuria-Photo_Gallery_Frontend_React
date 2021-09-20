c3e98827d2cee2ead4b18610691932e243e25c7e
José Aljuria <josejavieral13@gmail.com>
2021-09-19 22:11:11 -0400

    debug 2

--- src/Helpers/ServerApiDirection.ts
+++ src/Helpers/ServerApiDirection.ts
@@ -1,2 +1,2 @@
-export const serverApiDirection = process.env.ServerApiDirection || 'http://localhost:4000';
-console.log(process.env.ServerApiDirection);
+export const serverApiDirection = process.env.SERVER_API || 'http://localhost:4000';
+console.log(process.env.SERVER_API);
